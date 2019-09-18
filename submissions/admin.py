d302b0aaf152b13a19bdd88832fd934f3be92c78
Kikelomo Otutuloro <kotutuloro@salesforce.com>
2019-09-18 16:35:56 -0700

    more admin view changes

--- submissions/admin.py
+++ submissions/admin.py
@@ -2,16 +2,33 @@ from django.contrib import admin
 
 from .models import Student, Submission
 
+class SubmissionInline(admin.TabularInline):
+	model = Submission
+	max_num = 3
+	show_change_link = True
+
+	ordering = ['-submitted_at']
+
+	def has_add_permission(self, req, obj):
+		return False
+	def has_change_permission(self, req, obj):
+		return False
+	def has_delete_permission(self, req, obj):
+		return False
+
 class StudentAdmin(admin.ModelAdmin):
+	inlines = [SubmissionInline]
+
 	list_display = ('discord_id', 'ddr_name', 'twitter', 'alum')
 	list_display_links = ('discord_id', 'ddr_name')
 	list_filter = ('alum', ) # TODO: maybe a filter about needing info
 	ordering = ('discord_id', )
 	search_fields = ['discord_id', 'ddr_name', 'twitter']
 
-	# TODO: form -> view recent submissions?
-
 class SubmissionAdmin(admin.ModelAdmin):
+	autocomplete_fields = ['student']
+	readonly_fields = ('submitted_at', 'submission_picture')
+
 	list_display = ('student', 'score', 'submitted_at')
 	list_display_links = ('score', )
 	list_filter = (
@@ -21,9 +38,6 @@ class SubmissionAdmin(admin.ModelAdmin):
 	list_select_related = ('student', )
 	ordering = ('-submitted_at', )
 	search_fields = ['student__discord_id', 'student__ddr_name']
-
-	autocomplete_fields = ['student']
-	readonly_fields = ('submitted_at', 'submission_picture')
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Submission, SubmissionAdmin)
