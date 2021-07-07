78ba659fad514c0d7ca74d52977f8c0c95dd4cb5
Kikelomo Otutuloro <kotutuloro@salesforce.com>
2021-07-07 01:42:50 -0700

    admin.py tabs to spaces

--- submissions/admin.py
+++ submissions/admin.py
@@ -18,55 +18,55 @@ class LimitModelFormset(BaseInlineFormSet):
         self.queryset = kwargs['queryset'].filter(**_kwargs).order_by('-id')[:5]
 
 class SubmissionInline(admin.TabularInline):
-	model = Submission
-	show_change_link = True
-	formset = LimitModelFormset
+    model = Submission
+    show_change_link = True
+    formset = LimitModelFormset
 
-	ordering = ['-submitted_at']
+    ordering = ['-submitted_at']
 
-	def has_add_permission(self, req, obj):
-		return False
-	def has_change_permission(self, req, obj):
-		return False
-	def has_delete_permission(self, req, obj):
-		return False
+    def has_add_permission(self, req, obj):
+        return False
+    def has_change_permission(self, req, obj):
+        return False
+    def has_delete_permission(self, req, obj):
+        return False
 
 class StudentAdmin(admin.ModelAdmin):
-	inlines = [SubmissionInline]
+    inlines = [SubmissionInline]
 
-	list_display = ('discord_id', 'ddr_name', 'twitter', 'level')
-	list_display_links = ('discord_id', 'ddr_name')
-	list_filter = ('level', )
-	ordering = ('discord_id', )
-	search_fields = ['discord_id', 'ddr_name', 'twitter']
+    list_display = ('discord_id', 'ddr_name', 'twitter', 'level')
+    list_display_links = ('discord_id', 'ddr_name')
+    list_filter = ('level', )
+    ordering = ('discord_id', )
+    search_fields = ['discord_id', 'ddr_name', 'twitter']
 
 class ChallengeAdmin(admin.ModelAdmin):
-	ordering = ('-week', )
-	search_fields = ['week', 'name']
+    ordering = ('-week', )
+    search_fields = ['week', 'name']
 
 class SubmissionAdmin(admin.ModelAdmin):
 
-	def submission_picture(self, obj):
-		return format_html(
-			'''<a target="_blank" href="{}">
-			<img src={} style="max-width:100%; max-height:700px">
-			</a>''',
-			obj.pic_url, obj.pic_url
-		)
+    def submission_picture(self, obj):
+        return format_html(
+            '''<a target="_blank" href="{}">
+            <img src={} style="max-width:100%; max-height:700px">
+            </a>''',
+            obj.pic_url, obj.pic_url
+        )
 
-	autocomplete_fields = ['student', 'challenge']
-	readonly_fields = ('submitted_at', 'submission_picture')
+    autocomplete_fields = ['student', 'challenge']
+    readonly_fields = ('submitted_at', 'submission_picture')
 
-	list_display = ('challenge', 'score', 'student', 'submitted_at')
-	list_display_links = ('score', )
-	list_filter = (
-		('challenge', admin.RelatedOnlyFieldListFilter),
-		'student__level',
-		('student', admin.RelatedOnlyFieldListFilter)
-	) # TODO: maybe also filter by verification
-	list_select_related = ('student', 'challenge')
-	ordering = ('-submitted_at', )
-	search_fields = ['student__discord_id', 'student__ddr_name', 'challenge']
+    list_display = ('challenge', 'score', 'student', 'submitted_at')
+    list_display_links = ('score', )
+    list_filter = (
+        ('challenge', admin.RelatedOnlyFieldListFilter),
+        'student__level',
+        ('student', admin.RelatedOnlyFieldListFilter)
+    ) # TODO: maybe also filter by verification
+    list_select_related = ('student', 'challenge')
+    ordering = ('-submitted_at', )
+    search_fields = ['student__discord_id', 'student__ddr_name', 'challenge']
 
 admin_site = SubmissionsAdminSite()
 
