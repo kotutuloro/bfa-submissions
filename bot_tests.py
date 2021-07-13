7204ba6ccb3d40e0c8de57cc37e61b35aeaf9656
Kikelomo Otutuloro <kotutuloro@salesforce.com>
2021-07-13 14:19:28 -0700

    test submit command

--- bot_tests.py
+++ bot_tests.py
@@ -56,6 +56,65 @@ async def test_bot_ignores_other_channels(test_bot):
 
 @pytest.mark.django_db(transaction=True)
 @pytest.mark.asyncio
+async def test_submit_creates_submission(test_bot):
+    sender = test_bot.guilds[0].members[0]
+
+    await database_sync_to_async(models.Challenge.objects.create)(week=1, name='week1')
+    student = await database_sync_to_async(models.Student.objects.create)(
+        discord_snowflake_id=sender.id,
+    )
+
+    await dpytest.message(content="!submit 1234", member=sender, attachments=["fake"])
+
+    subm = await database_sync_to_async(models.Submission.objects.first)()
+    assert subm.score == 1234
+    assert subm.challenge_id == 1
+    assert subm.student_id == student.id
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_submit_requires_attachment(test_bot):
+    await database_sync_to_async(models.Challenge.objects.create)(week=1, name='week1')
+
+    with pytest.raises(commands.UserInputError):
+        await dpytest.message(content="!submit 1234")
+    assert dpytest.verify().message().contains().content("needs one file attachment")
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_submit_creates_student(test_bot):
+    await database_sync_to_async(models.Challenge.objects.create)(week=1, name='week1')
+
+    sender = await make_role_member(test_bot, "Freshman")
+    await dpytest.message(content="!submit 1234", member=sender, attachments=["fake"])
+
+    student = await database_sync_to_async(models.Student.objects.get)(discord_snowflake_id=sender.id)
+    assert student is not None
+    assert student.discord_snowflake_id == sender.id
+    assert student.discord_name == f'{sender.name}#{sender.discriminator}'
+    assert student.level == models.Student.LevelPlacement.FRESHMAN
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_submit_updates_student(test_bot):
+    new_grad = await make_role_member(test_bot, "Graduate")
+
+    await database_sync_to_async(models.Challenge.objects.create)(week=1, name='week1')
+    existing_student = await database_sync_to_async(models.Student.objects.create)(
+        discord_snowflake_id=new_grad.id,
+        discord_name="xyz#123",
+        level=models.Student.LevelPlacement.VARSITY,
+    )
+
+    await dpytest.message(content="!submit 1234", member=new_grad, attachments=["fake"])
+    student = await database_sync_to_async(models.Student.objects.get)(id=existing_student.id)
+
+    assert student.discord_snowflake_id == new_grad.id
+    assert student.discord_name == f'{new_grad.name}#{new_grad.discriminator}'
+    assert student.level == models.Student.LevelPlacement.GRADUATE
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
 async def test_newweek_creates_given_week(test_bot):
     admin = await make_role_member(test_bot, "Admin")
 
